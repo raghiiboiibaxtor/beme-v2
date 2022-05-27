5a56e0dc626b4d146e389cccc647b00ba2c5820a
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-05-27 21:21:49 +1200

    Firestore: AddData

---
+++ src/components/forms/adduserform.svelte
@@ -0,0 +1,23 @@
+<script>
+    import {firestore} from "./firebase/firebase.js";
+    import {
+      collection,
+      addDoc,
+    } from "firebase/firestore";
+
+
+    const addTask = async () => {
+      try {
+        await addDoc(collection(firestore, "tasks"), {
+          ...task,
+          createdAt: Date.now(),
+        });
+        Toastify({
+          text: "New Task created",
+        }).showToast();
+      } catch (error) {
+        console.error(error);
+      }
+    };
+</script>
+
