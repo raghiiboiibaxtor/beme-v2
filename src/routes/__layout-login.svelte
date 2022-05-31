41a3c7e00758a6c2ee984ef62003191ef0693a15
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-01 11:33:56 +1200

    Code Cleanup

--- src/routes/__layout-login.svelte
+++ src/routes/__layout-login.svelte
@@ -1,38 +1,4 @@
-<script>
-  //import "../app.css";
-  // Importing SDKs
-  import App from "./firebase/firebase.js";
-  import { onMount } from "svelte";
-  import { getAuth, onAuthStateChanged } from "firebase/auth";
-  import { goto } from "$app/navigation";
-  import { isLoggedIn } from "./stores/authstore.js";
-  //import Navbar from "../components/layout/navbar.svelte";
-  //import Sidebar from "../components/layout/sidebar.svelte";
- // import Login from "./authenticate/login.svelte";
-  import {page} from '$app/stores';
-  
-  let login = false;
-  let url = $page.url.pathname;
-  // Mounting authentication.
-  // Grabbing getAuth() variable from Firebase and passing it locally to auth
-  onMount(() => {
-    const auth = getAuth();
-    // Monitors authentication state changes.
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        // If user verified open application
-        console.log("Welcome to the BeMe!");
-        isLoggedIn.update(() => true);
-        login = true; // If the user exists authstore writeable will return true.
-      } else {
-        // Else redirect to login page
-        isLoggedIn.update(() => false); // Ensuring authstore writable is false and user cannot login
-        goto("/authenticate/login"); // Navigating back to login page
-        login = false; 
-      }
-    });
-  });
-</script>
+
 <slot />
 
 
