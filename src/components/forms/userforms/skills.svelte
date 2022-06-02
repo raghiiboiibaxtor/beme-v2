c35cd1ae57c53b8e21225a48ca8a9b5eca0f4ef5
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-02 23:08:38 +1200

    Profile Page (AddInfo) Complete
    The add user detail forms are complete and responsive. Form css made global.

--- src/components/forms/userforms/skills.svelte
+++ src/components/forms/userforms/skills.svelte
@@ -1,5 +1,5 @@
 <script>
-    
+    import './formsglobal.css';
     // add data
     import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
     import {
@@ -68,8 +68,8 @@
 <div class="body-container">
     <div class="form-container">
     <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
-    <h1 class="form-heading"> Skills </h1>
-    <h1 class="form-subtitle"> The things that make you good at what you do!</h1>
+    <h1 class="form-heading"> Professional Skills </h1>
+    <h1 class="form-subtitle"> The only toolbox you need.</h1>
     <form on:submit|preventDefault={handleSubmit} class="form-card">
         <div class="input-title-flex">
         <label for="title" class="label-input">Personal Favourite</label>
@@ -161,168 +161,3 @@
     </form>
     </div>
 </div>
-
-<style>
-
-    .body-container{
-        display: flex;
-        flex-direction: column;
-        margin-left: 1.23rem;
-        padding-top: 1.23rem;
-        width: 444px;
-    
-    }
-
-
-    .form-container{
-        width: 100%;
-        height: auto; 
-        display: flex;
-        flex-direction: column;
-        align-items: left;
-        justify-content: space-between;
-        margin-bottom: 1.23rem;
-        padding: 1.23rem;
-        background-color: var(--light);
-        border: 2.23px solid rgba(112, 156, 212, 0.0);
-        background: 
-      linear-gradient(var(--light), var(--light)) padding-box,
-      linear-gradient(90deg, #5271FF 0%, #38B6FF 100%) border-box;
-        box-shadow: 1px 4px 4px rgba(82, 101, 165, 0.12);
-        border-radius: 44px;
-    }
-
-    .form-heading{
-        font-family: 'Inter';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 33px;
-        letter-spacing: -0.02em;
-        background: -webkit-linear-gradient(45deg, #5271FF, #38B6FF);
-        -webkit-background-clip: text;
-        -webkit-text-fill-color: transparent;
-    }
-
-    .form-subtitle{
-        font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 400;
-        font-size: 16px;
-        line-height: 119.7%;
-        letter-spacing: -0.02em;
-        color: var(--dark);
-    }
-
-    .form-card{
-        margin-top: 0.55rem;
-    }
-
-    .input-title-flex{
-        margin-top: 0.55rem;
-    }
-
-    .label-input{
-        margin-top: 0.23rem;
-        font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 700;
-        font-size: 14px;
-        letter-spacing: 0.23px;
-        color: var(--placeholdertext);
-    }
-
-    .label-p-input{
-      margin-top: 0.23rem;
-      margin-bottom: 0.55rem;
-      font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 10px;
-        line-height: 119.7%;
-        letter-spacing: -0.02em;
-        color: var(--dark);
-    }
-
-    input{
-        font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 500;
-        height: 2.23rem;
-        border-radius: 23rem;
-        border: 2.23px solid rgba(112, 156, 212, 0.44);
-        width: 100%;
-        color: var(--main);
-        font-size: 14px;
-    }
-
-    input::placeholder{
-        color: var(--placeholdertext);
-    }
-
-    input:focus{
-        outline-style: none;
-	    box-shadow: none;
-	    border-color: transparent;
-        background: 
-      linear-gradient(var(--light), var(--light)) padding-box,
-      linear-gradient(90deg, #5271FF 0%, #38B6FF 100%) border-box;
-    }
-
-    .skill-flex{
-      width: 100%;
-      display: flex;
-      justify-content: space-between;
-      margin-bottom: 0.55rem;
-      margin-top: 0.23rem;
-    }
-
-    .left-items{
-      width: 48%;
-    }
-
-    .right-items{
-      width: 48%;
-    }
-
-    .skill-form-control{
-      font-family: 'Montserrat';
-        font-style: normal;
-        font-weight: 500;
-        height: 2.23rem;
-        border-radius: 23rem;
-        border: 2.23px solid rgba(112, 156, 212, 0.44);
-        width: 100%;
-        color: var(--main);
-        font-size: 12px;
-    }
-
-
-    .button-flex{
-        display: flex;
-        justify-content: flex-end;
-        margin-top: 0.55rem;
-    }
-    
-    .button{
-    font-family: 'Montserrat';
-    font-style: normal;
-    font-weight: 700;
-    letter-spacing: 0.23px;
-    width: 8.8rem;
-    font-size: large;
-    color: white;
-    background-image: linear-gradient(
-    45deg,
-    hsl(237deg 81% 60%) 0%,
-    hsl(218deg 94% 54%) 20%,
-    hsl(210deg 100% 50%) 40%,
-    hsl(205deg 100% 50%) 60%,
-    hsl(201deg 100% 50%) 80%,
-    hsl(202deg 100% 61%) 100% );
-    border-radius: 23rem;
-    text-align: center !important;
-  }
-    
-
-
-</style>
