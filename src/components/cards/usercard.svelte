ffb71cb136d293a299fd2b97bf43875e8930c41a
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-11 23:08:40 +1200

    CommentsAdded, CodeCleanUP
    DynamicReadUserFromFirestore() working, commented and cleaned

--- src/components/cards/usercard.svelte
+++ src/components/cards/usercard.svelte
@@ -3,6 +3,7 @@
     import {onSnapshot, doc, getDoc, collection, query, where,} from "firebase/firestore";
     import {_firestore_} from "../../routes/firebase/firebase.js"; 
 
+    // Grabbing auth from firebase
     const auth = getAuth();
     let _uid;
     let _email;
@@ -10,12 +11,13 @@
     if (user) {
         _uid = user.uid;
         _email = user.email;
-        // ...
+        // If user is authenticated, assign their data into global variables.
     } else {
-        // User is signed out
+        // User is signed out (logic to be included here)
     }
     });
 
+    // Creating struct / list
     let _info = {
         firstname: "",
         lastname: "",
@@ -24,35 +26,42 @@
         personalbio: "",
     };
 
+    // Creating array 
     let _userdetails = [];
 
-  // Reading Multiple Documents from Firestore
+    //Reading Collection 'AllUsers' from Firestore
     const _collection = collection(_firestore_, "AllUsers");
-
+    // Copying Collection Data into a "snapshot"
     const _snapshot = onSnapshot(_collection, (querySnapshot) => {
-      let _fireuser = [];
+      let _fireuser = []; // Local array _fireuser
       querySnapshot.forEach((doc) => {
-          let details = {...doc.data(), userid: doc.id}
-          _fireuser = [details, ..._fireuser];
-          _userdetails.push(doc.data());
-          _userdetails = _fireuser;
-          _userid = doc.data().userid;
+          let details = {...doc.data(), userid: doc.id} // Defining details which will be pushed into the local array _fireuser
+          _fireuser = [details, ..._fireuser]; // Spreding the newly assigned data into _fireuser
+          _userdetails.push(doc.data()); // Pushing firestore document data to _userdetails[]
+          _userdetails = _fireuser; // Assigning local[] data to global[] 
+          _userid = doc.data().userid; // Grabbing user id from doc and assigning it to global variable (which will be used in if condition)
       });
         console.log(_fireuser)
     });
   
 </script>
 
-<!-- ######################## HTML BEGINS ############################### -->
+<!-- HTML BEGINS -->
 
-<h1> ****************************************</h1>
 <h1> **Reading Dynamic Data From Firestore**</h1>
-{#each _userdetails as _info}
 
-{#if _info.userid == _uid && _info.email == _email}
+<!-- Each loop, executing the _userdetails[] array -->
+{#each _userdetails as _info}
+<!-- If block, finding the relevant userdata based on document auth -->
+{#if _info.userid == _uid && _info.email == _email} 
+<!-- Correct data displaying -->
 <p>{_info.userid}</p>
-<p>{_info.firstname}</p>
 <p>{_info.email}</p>
+<p>{_info.firstname}</p>
+<p>{_info.lastname}</p>
+<p>{_info.pronouns}</p>
+<p>{_info.workemail}</p>
+<p>{_info.bio}</p>
 {/if}
  
 {/each}
