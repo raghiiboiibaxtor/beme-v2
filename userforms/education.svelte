8809199af0ed358a86b90e2bd038b3b7e5709ab3
Nolene Human <81940094+270122090@users.noreply.github.com>
2022-06-12 17:49:01 +1200

    UpdateDoc
    Updated Userprofile, Skiolls, eduction and aboutmeform with database structure and updateDoc function for current signed in user

---
+++ userforms/education.svelte
@@ -0,0 +1,111 @@
+<script>
+    import './formsglobal.css';
+    // add data
+    import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
+    import {updateDoc,onSnapshot,doc} from "firebase/firestore";
+    import { getAuth,onAuthStateChanged} from "firebase/auth";
+
+
+    // Creating list of variables that will pass data to Firestore ()
+    let _education= {
+      _education:{qualification: "",
+      study_level: "",
+      study_place: "",
+      fav_subject: "",
+      year: ""}};
+
+    let inputElement; // Declaring input element used for binding ui to list variables
+
+    const handleSubmit = () => {
+    
+    const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+        //successfully logging the current user signed ins data on console
+        const _uid = user.uid;       
+        
+
+      // console log reference of current user signed in
+        onSnapshot(doc(_firestore_, "AllUsers", _uid), (doc) => {
+          console.log("Current user logged in: ", doc.data());})
+
+     //we only need update, since the user gets their data created / addedwhen they sign up, 
+      const _userupdate = doc(_firestore_, 'AllUsers', _uid)
+
+      updateDoc(_userupdate,{
+        ..._education})        
+    }})     
+    inputElement.focus();}
+
+</script>
+
+<!-- HTML TEMPLATE BEGINS -->
+<div class="body-container">
+    <div class="form-container">
+    <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
+    <h1 class="form-heading"> Education </h1>
+    <h1 class="form-subtitle"> Life is all about learning, then learning again.</h1>
+    <form on:submit|preventDefault={handleSubmit} class="form-card">
+        <div class="input-title-flex">
+        <label for="title" class="label-input">Qualification</label>
+        <!-- Binding firestore variables to ui and grabbing user entered text -->
+        <input
+            type="text"
+            bind:value={_education._education.qualification}
+            bind:this={inputElement}
+            placeholder= "What did you graduate with?"
+            class="form-control"
+        />
+        </div>
+
+        <div class="input-title-flex">
+          <label for="title" class="label-input">Level of Study</label>
+          <!-- Binding firestore variables to ui and grabbing user entered text -->
+          <input
+              type="text"
+              bind:value={_education._education.study_level}
+              bind:this={inputElement}
+              placeholder= "Eg: NZQA Level 8"
+              class="form-control"
+          />
+          </div>
+
+          <div class="input-title-flex">
+            <label for="title" class="label-input">Place of Study</label>
+            <input
+                type="text"
+                bind:value={_education._education.study_place}
+                bind:this={inputElement}
+                placeholder= "Where did you study?"
+                class="form-control"
+            />
+            </div>
+    
+        <div class="input-title-flex">
+        <label for="description" class="label-input">Favourite Subject</label>
+        <input
+            type="text"
+            bind:value={_education._education.fav_subject}
+            bind:this={inputElement}
+            placeholder="What did you enjoy learning the most?"
+            class="form-control"
+        />
+        </div>
+
+        <div class="input-title-flex">
+          <label for="description" class="label-input">Graduation Year </label>
+          <input
+              type="text"
+              bind:value={_education._education.year}
+              bind:this={inputElement}
+              placeholder="When did you graduate?"
+              class="form-control"
+          />
+          </div>
+       
+        <div class="button-flex">
+        <button class="button" >Save </button>
+        </div>
+    </form>
+    </div>
+</div>
---
+++ userforms/userprofile.svelte
@@ -0,0 +1,119 @@
+<script>
+    import './formsglobal.css';
+    // add data
+    import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
+    import {updateDoc,onSnapshot,doc, getDoc,runTransaction} from "firebase/firestore";
+    import { getAuth,onAuthStateChanged} from "firebase/auth";
+
+    // Creating list of variables that will pass data to Firestore ()
+    let _myprofile = {
+      _profile:{firstname: "",
+      lastname: "",
+      email: "",
+      phone: "",
+      bio:""}};
+
+      
+     let _showprofile=["name","surname","email"]
+
+
+    let inputElement;
+
+    const handleSubmit = () => {
+    
+    const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+        //successfully logging the current user signed ins data on console
+        const _uid = user.uid;       
+        
+
+      // console log reference of current user signed in
+        onSnapshot(doc(_firestore_, "AllUsers", _uid), (doc) => {
+          console.log("Current user logged in: ", doc.data());})
+
+     //we only need update, since the user gets their data created / addedwhen they sign up, 
+      const _userupdate = doc(_firestore_, 'AllUsers', _uid)
+
+      updateDoc(_userupdate,{
+        ..._myprofile})        
+    }})     
+    inputElement.focus();}    // Declaring input element used for binding ui to list variables
+
+</script>
+
+
+
+<!-- HTML TEMPLATE BEGINS -->
+
+
+<div class="body-container">
+    <div class="form-container">
+    <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
+    <h1 class="form-heading"> My Profile</h1>
+    <h1 class="form-subtitle"> Enter your details to get started.</h1>
+    <form on:submit|preventDefault={handleSubmit} class="form-card">
+        <div class="input-title-flex">
+        <label for="title" class="label-input">First Name</label>
+        <!-- Binding firestore variables to ui and grabbing user entered text -->
+        <input
+            type="text"
+            bind:value={_showprofile[0]}
+            placeholder={_showprofile[0]}
+            class="form-control"
+        />
+        </div>
+
+        <div class="input-title-flex">
+        <label for="description" class="label-input">Last Name</label>
+        <input
+            type="text"
+            bind:value={_myprofile._profile.lastname}
+            bind:this={inputElement}
+            placeholder="Enter your last name"
+            class="form-control"
+        />
+
+        <div class="input-title-flex">
+          <label for="description" class="label-input"
+              >Email</label
+          >
+          <input
+              type="text"
+              bind:value={_myprofile._profile.email}
+              bind:this={inputElement}
+              placeholder="Enter your email."
+              class="form-control"
+          />
+          </div>
+          <div class="input-title-flex">
+            <label for="description" class="label-input"
+                >Phone Number</label
+            >
+            <input
+                type="text"
+                bind:value={_myprofile._profile.phone}
+                bind:this={inputElement}
+                placeholder="Enter your contact number."
+                class="form-control"
+            />
+            </div>
+
+        <div class="input-title-flex">
+        <label for="description" class="label-input"
+            >Bio</label
+        >
+        <input
+            type="text"
+            bind:value={_myprofile._profile.bio}
+            bind:this={inputElement}
+            placeholder="This is your elevator pitch"
+            class="textarea-form-control"
+        />
+        </div>
+        <div class="button-flex">
+        <button class="button" >Save </button>
+        </div>
+    </form>
+    </div>
+</div>
