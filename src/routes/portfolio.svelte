065fc15d4d60b9f19cd43f93efb27fe2b79a22d3
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-13 21:41:47 +1200

    EditDeleteData()
    Code working, in temporary file updatedata.svelte. Needs to be integrated into ui

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -1,7 +1,9 @@
 <script>
    // import AddImage from "../content/addimage.svelte";
    import UserCard from "../components/cards/usercard.svelte";
+   import UpdateData from "../components/forms/updatedata.svelte";
 </script>
 
 <h1>This is the portfolio page</h1>
 <UserCard/>
+<UpdateData/>
