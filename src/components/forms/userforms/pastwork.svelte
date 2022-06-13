2f279f3d43583e7db330a89f9e10499656416d8e
Nolene Human <81940094+270122090@users.noreply.github.com>
2022-06-13 19:25:43 +1200

    newuser-update -cards
    the update data cards is working

--- src/components/forms/userforms/pastwork.svelte
+++ src/components/forms/userforms/pastwork.svelte
@@ -1,117 +1,114 @@
 <script>
-    import './formsglobal.css';
-    // add data
-    import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
-    import {collection, addDoc,} from "firebase/firestore";
+  import './formsglobal.css';
+  // add data
+  import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
+  import {updateDoc,onSnapshot,doc} from "firebase/firestore";
+  import { getAuth,onAuthStateChanged} from "firebase/auth";
 
+  // Creating list of variables that will pass data to Firestore ()
+  let _experience = {
+    _experience1:{role: "",
+    company: "",
+    date: "",
+    skill_applied:""},
+    _experience2:{role: "",
+    company: "",
+    date: "",
+    skill_applied:""},
+    _experience3:{role: "",
+    company: "",
+    date: "",
+    skill_applied:""},
+    _experience4:{role: "",
+    company: "",
+    date: "",
+    skill_applied:""}
+  };
 
-    // Creating list of variables that will pass data to Firestore ()
-    let _info = {
-      role: "",
-      company: "",
-      time: "",
-      reference: "",
-      contact: "",
-    };
+  let inputElement; // Declaring input element used for binding ui to list variables
 
-    let inputElement; // Declaring input element used for binding ui to list variables
+  const handleSubmit = () => {
+  
+  const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+      //successfully logging the current user signed ins data on console
+      const _uid = user.uid;       
+      
 
-    /* Adding User to Firestore */ 
-    const addUserToFirestore = async () => {
-      try {
-        await addDoc(collection(_firestore_, "AllUsers"), {
-          ..._info,
-          createdAt: Date.now(),
-        });
-        // Waiting to catch errors
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    // console log reference of current user signed in
+      onSnapshot(doc(_firestore_, "AllUsers", _uid), (doc) => {
+        console.log("Current user logged in: ", doc.data());})
 
-    // Handling form submission & passing addUserToFirestore()
-    const handleSubmit = () => {
-      addUserToFirestore();
-      _info = { role: "",
-      company: "",
-      time: "",
-      reference: "",
-      contact: "",
-      };
-      inputElement.focus();
-    };
+   //we only need update, since the user gets their data created / addedwhen they sign up, 
+    const _userupdate = doc(_firestore_, 'AllUsers', _uid)
+
+    updateDoc(_userupdate,{
+      ..._experience})        
+  }})     
+  inputElement.focus();}   
 
 </script>
 
 
 
 <!-- HTML TEMPLATE BEGINS -->
+
+
 <div class="body-container">
-    <div class="form-container">
-    <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
-    <h1 class="form-heading"> Past Work </h1>
-    <h1 class="form-subtitle"> The journey that got you here.</h1>
-    <form on:submit|preventDefault={handleSubmit} class="form-card">
-        <div class="input-title-flex">
-        <label for="title" class="label-input">Job Title</label>
+<div class="form-container">
+  <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
+  <h1 class="form-heading"> Detailed Experience </h1>
+  <h1 class="form-subtitle"> The journey that got you here.</h1>
+  <form on:submit|preventDefault={handleSubmit} class="form-card">
+      <div class="input-title-flex">
+      <label for="title" class="label-input">Job Title</label>
+      <!-- Binding firestore variables to ui and grabbing user entered text -->
+      <input
+          type="text"
+          bind:value={_experience._experience1.role}
+          bind:this={inputElement}
+          placeholder= "What was your job title?"
+          class="form-control"
+      />
+      </div>
+
+      <div class="input-title-flex">
+        <label for="title" class="label-input">Place / Company</label>
         <!-- Binding firestore variables to ui and grabbing user entered text -->
         <input
             type="text"
-            bind:value={_info.role}
+            bind:value={_experience._experience1.company}
             bind:this={inputElement}
-            placeholder= "What was your job title?"
+            placeholder= "What company did you experience for?"
             class="form-control"
         />
         </div>
 
         <div class="input-title-flex">
-          <label for="title" class="label-input">Workplace / Company</label>
-          <!-- Binding firestore variables to ui and grabbing user entered text -->
+          <label for="title" class="label-input">Duration</label>
           <input
               type="text"
-              bind:value={_info.company}
+              bind:value={_experience._experience1.date}
               bind:this={inputElement}
-              placeholder= "What company did you work for?"
+              placeholder= "How long did you experience here?"
               class="form-control"
           />
-          </div>
-
-          <div class="input-title-flex">
-            <label for="title" class="label-input">Length of Time</label>
-            <input
-                type="text"
-                bind:value={_info.time}
-                bind:this={inputElement}
-                placeholder= "How long did you work?"
-                class="form-control"
-            />
-            </div>
-    
+          </div>            
         <div class="input-title-flex">
-        <label for="description" class="label-input">Reference</label>
-        <input
-            type="text"
-            bind:value={_info.reference}
-            bind:this={inputElement}
-            placeholder="Who's your reference?"
-            class="form-control"
-        />
-        </div>
-
-        <div class="input-title-flex">
-          <label for="description" class="label-input">Reference Contact</label>
+          <label for="title" class="label-input">Skills Applied</label>
           <input
               type="text"
-              bind:value={_info.contact}
+              bind:value={_experience._experience1.skill_applied}
               bind:this={inputElement}
-              placeholder="What's their email?"
-              class="form-control"
+              placeholder= "What skills did you applied or learned."
+              class="textarea-form-control"
           />
-          </div>
-       
-        <div class="button-flex">
-        <button class="button" >Save </button>
-        </div>
-    </form>
-    </div>
+          </div>       
+      <div class="button-flex">
+      <button class="button" >Save </button>
+      </div>
+  </form>
+  </div>
 </div>
+
--- src/components/forms/userforms/userprofile.svelte
+++ src/components/forms/userforms/userprofile.svelte
@@ -1,45 +1,44 @@
 <script>
-    import './formsglobal.css';
-    // add data
-    import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
-    import {collection, addDoc,} from "firebase/firestore";
+  import './formsglobal.css';
+  // add data
+  import {_firestore_} from "../../../routes/firebase/firebase.js"; //Importing Firestore component that was initialised in firebase.js
+  import {updateDoc,onSnapshot,doc, getDoc,runTransaction} from "firebase/firestore";
+  import { getAuth,onAuthStateChanged} from "firebase/auth";
+
+  // Creating list of variables that will pass data to Firestore ()
+  let _myprofile = {
+    _profile:{firstname: "",
+    lastname: "",
+    email: "",
+    phone: "",
+    bio:""}};
+
+    
+   let _showprofile=["name","surname","email"]
 
 
-    // Creating list of variables that will pass data to Firestore ()
-    let _info = {
-      firstname: "",
-      lastname: "",
-      workemail: "",
-      pronouns: "",
-      personalbio: "",
-    };
+  let inputElement;
 
-    let inputElement; // Declaring input element used for binding ui to list variables
+  const handleSubmit = () => {
   
-    /* Adding User to Firestore */ 
-    const addUserToFirestore = async () => {
-      try {
-        await addDoc(collection(_firestore_, "AllUsers"), {
-          ..._info,
-          createdAt: Date.now(),
-        });
-        // Waiting to catch errors
-      } catch (error) {
-        console.error(error);
-      }
-    };
+  const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+      //successfully logging the current user signed ins data on console
+      const _uid = user.uid;       
+      
 
-   // Handling form submission & passing addUserToFirestore()
-    const handleSubmit = () => {
-      addUserToFirestore();
-      _info = {firstname: "",
-      lastname: "",
-      workemail: "",
-      pronouns: "",
-      personalbio: "",
-      };
-      inputElement.focus();
-    };
+    // console log reference of current user signed in
+      onSnapshot(doc(_firestore_, "AllUsers", _uid), (doc) => {
+        console.log("Current user logged in: ", doc.data());})
+
+   //we only need update, since the user gets their data created / addedwhen they sign up, 
+    const _userupdate = doc(_firestore_, 'AllUsers', _uid)
+
+    updateDoc(_userupdate,{
+      ..._myprofile})        
+  }})     
+  inputElement.focus();}    // Declaring input element used for binding ui to list variables
 
 </script>
 
@@ -49,76 +48,72 @@
 
 
 <div class="body-container">
-    <div class="form-container">
-    <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
-    <h1 class="form-heading"> My Profile</h1>
-    <h1 class="form-subtitle"> Enter your details to get started.</h1>
-    <form on:submit|preventDefault={handleSubmit} class="form-card">
-        <div class="input-title-flex">
-        <label for="title" class="label-input">First Name</label>
-        <!-- Binding firestore variables to ui and grabbing user entered text -->
-        <input
-            type="text"
-            bind:value={_info.firstname}
-            bind:this={inputElement}
-            placeholder= 'Enter your first name'
-            class="form-control"
-        />
-        </div>
+  <div class="form-container">
+  <!-- Task Form  handleSubmit() called when form is submitted. Default form response prevented-->
+  <h1 class="form-heading"> My Profile</h1>
+  <h1 class="form-subtitle"> Enter your details to get started.</h1>
+  <form on:submit|preventDefault={handleSubmit} class="form-card">
+      <div class="input-title-flex">
+      <label for="title" class="label-input">First Name</label>
+      <!-- Binding firestore variables to ui and grabbing user entered text -->
+      <input
+          type="text"
+          bind:value={_showprofile[0]}
+          placeholder={_showprofile[0]}
+          class="form-control"
+      />
+      </div>
 
-        <div class="input-title-flex">
-        <label for="description" class="label-input">Last Name</label>
-        <input
-            type="text"
-            bind:value={_info.lastname}
-            bind:this={inputElement}
-            placeholder="Enter your last name"
-            class="form-control"
-        />
-        </div>
+      <div class="input-title-flex">
+      <label for="description" class="label-input">Last Name</label>
+      <input
+          type="text"
+          bind:value={_myprofile._profile.lastname}
+          bind:this={inputElement}
+          placeholder="Enter your last name"
+          class="form-control"
+      />
 
-        <div class="input-title-flex">
+      <div class="input-title-flex">
         <label for="description" class="label-input"
-            >Pronouns</label
+            >Email</label
         >
         <input
             type="text"
-            bind:value={_info.pronouns}
+            bind:value={_myprofile._profile.email}
             bind:this={inputElement}
-            placeholder="What are your pronouns?"
+            placeholder="Enter your email."
             class="form-control"
         />
         </div>
-
         <div class="input-title-flex">
-        <label for="description" class="label-input"
-            >Personal Bio</label
-        >
-        <input
-            type="text"
-            bind:value={_info.personalbio}
-            bind:this={inputElement}
-            placeholder="Keep it short and sweet!"
-            class="form-control"
-        />
-        </div>
+          <label for="description" class="label-input"
+              >Phone Number</label
+          >
+          <input
+              type="text"
+              bind:value={_myprofile._profile.phone}
+              bind:this={inputElement}
+              placeholder="Enter your contact number."
+              class="form-control"
+          />
+          </div>
 
-        <div class="input-title-flex">
-            <label for="description" class="label-input"
-                >Professional Email</label
-            >
-            <input
-                type="text"
-                bind:value={_info.workemail}
-                bind:this={inputElement}
-                placeholder="Enter your professional email."
-                class="form-control"
-            />
-            </div>
-
-        <div class="button-flex">
-        <button class="button" >Save </button>
-        </div>
-    </form>
-    </div>
+      <div class="input-title-flex">
+      <label for="description" class="label-input"
+          >Bio</label
+      >
+      <input
+          type="text"
+          bind:value={_myprofile._profile.bio}
+          bind:this={inputElement}
+          placeholder="This is your elevator pitch"
+          class="textarea-form-control"
+      />
+      </div>
+      <div class="button-flex">
+      <button class="button" >Save </button>
+      </div>
+  </form>
+  </div>
 </div>
