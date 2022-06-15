1daeec5e7eb6b51c7dcf88dc0ed8bed59857d219
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-15 18:20:38 +1200

    FileNull, Title adding

--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -2,8 +2,10 @@
    // import AddImage from "../content/addimage.svelte";
    import UserCard from "../components/cards/usercard.svelte";
    import UpdateData from "../components/forms/updatedata.svelte";
+   import AddImage from "../content/newaddimage.svelte";
 </script>
 
 <h1>This is the portfolio page</h1>
 <UserCard/>
 <UpdateData/>
+<AddImage/>
