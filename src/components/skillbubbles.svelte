ec2a6cc7e0301deaaa8a75c1eb88ae68ded7446e
Raghiiboii Baxtor <85328806+raghiiboiibaxtor@users.noreply.github.com>
2022-06-19 18:25:57 +1200

    Profile Cards UI

---
+++ src/components/skillbubbles.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let _skill;
+</script>
+
+<div class="skill-container">
+    <div class="skill-bubble">
+<h1 class="skill-text">{_skill}</h1>
+</div>
+</div>
+
+<style>
+    .skill-container{
+        margin-top: 0.88rem;
+    }
+    .skill-bubble{
+        max-width: auto;
+       margin: auto;
+        padding: 0.23rem 0.88rem;
+        border: 1.23px solid rgba(112, 156, 212, 0.0);
+        background: 
+            linear-gradient(var(--light), var(--light)) padding-box,
+            linear-gradient(90deg, #5271FF 0%, #38B6FF 100%) border-box;
+            box-shadow: 1px 4px 4px rgba(82, 101, 165, 0.055);
+            border-radius: 23px;
+        width: auto;
+    }
+
+    .skill-text{
+        font-family: 'Inter';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 18px;
+        line-height: 119.7%;
+        /* or 22px */
+
+        display: flex;
+        align-items: center;
+        text-align: center;
+
+        color: rgba(2, 81, 177, 0.77);
+
+    }
+</style>
--- src/routes/portfolio.svelte
+++ src/routes/portfolio.svelte
@@ -6,10 +6,19 @@
    import AddImage from "../content/newaddimage.svelte";
   import AddFile from "../components/files/addfile.svelte";
   import CardFavExp from "../components/cards/card_favexp.svelte";
+  import CardEducation from "../components/cards/card_education.svelte";
+  import CardPaswork from "../components/cards/card_paswork.svelte";
+  import CardSkills from "../components/cards/card_skills.svelte";
+
+
 </script>
+
 
 <UserCard/>
 <CardFavExp/>
+<CardEducation/>
+<CardPaswork/>
+<CardSkills/>
 <!--
 <UpdateData/>
 <AddFile/>
